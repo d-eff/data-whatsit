07e71a12b2646f1e9340ecadbe457ccc6a62b935
David Fernandez <david.fernandez@showtime.net>
2020-04-23 03:42:45 -0400

    best years of my life wasted on nested json

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let tableContainer = document.querySelector('#tableWrap');
 fileEle.addEventListener('change', onFileChange);
 
 let data = [];
-let headers = [];
+let headers = {};
 
 function onFileChange() {
   //reset
@@ -38,13 +38,12 @@ function onFileChange() {
         } else {
           //we're done with this, reset it now in case we fail parsing
           fileEle.value = null;
-
-          progress.innerHTML += `<p>LOAD COMPLETE.</p>`;
           data = parseFileData(chunks);
 
           //don't need this if we can get the headers loaded in via separate file
           headers = getHeaders(data);
 
+          //todo: yes we store them globally but I think I have a plan
           buildTable(data, headers);
         };
       }
@@ -56,55 +55,88 @@ function onFileChange() {
 }
 
 function parseFileData(chunks) {
-  // progress.innerHTML += `<p>PARSING...</p>`;
-  let content = chunks.join("");
+  let content = chunks.join('');
   let contentArr = content.split('\n');
 
   return contentArr.map(ele => {
     try {
       return JSON.parse(ele);
     } catch (e) {
-      return "";
+      return '';
     }
-  }).filter(ele => typeof ele === "object");
+  }).filter(ele => typeof ele === 'object');
+}
+
+function getHeadersFromSample(data) {
+  let headers = {};
+
+  let keys = Object.keys(data);
+  keys.forEach(key => {
+    let item = data[key];
+    headers[key] = (typeof item === 'object' && !Array.isArray(item)) ? getHeadersFromSample(data[key]) : '';
+  });
+
+  return headers;
 }
 
 function getHeaders(data) {
-  // progress.innerHTML += `<p>GETTING HEADERS...</p>`;
-  let headers = [];
+  let headers = {};
 
   data.forEach((ele, idx) => {
-    let keys = Object.keys(ele);
-    if (keys.length > headers.length) {
-      headers = keys;
+    let sampleHeaders = getHeadersFromSample(ele);
+    //this is all hilariously costly, I think
+    if (JSON.stringify(headers) !== JSON.stringify(sampleHeaders)) {
+      headers = Object.assign(headers, sampleHeaders);
     }
   });
 
   return headers;
 }
 
-function buildTable(data, headers) {
-  // progress.innerHTML += `<p>BUILDING TABLE...</p>`;
+function buildTableHead(headers) {
   let thead = '';
-  headers.forEach(ele => {
-    //todo: is '' to `` a cast? how are those interpreted?
-    thead += `<th>${ele}</th>`
+  let headKeys = Object.keys(headers);
+  headKeys.forEach(ele => {
+    //minor: is '' to `` a cast? how are those interpreted?
+    //todo: TCO all this recursion
+    if (typeof headers[ele] === 'object') {
+      thead += buildTableHead(headers[ele]);
+    } else {
+      thead += `<th>${ele}</th>`
+    }
   })
-  
+  return thead;
+}
+
+function buildTableRow(data, headers) {
+  let row = '';
+  let headKeys = Object.keys(headers);
+  headKeys.forEach(header => {
+    if (typeof headers[header] === 'object') {
+      row += buildTableRow(data[header] || {}, headers[header]);
+    } else {
+      row += `<td>${data[header] || ''}</td>`;
+    }
+  });
+  return row;
+}
+
+function buildTableBody(data, headers) {
   let tbody = '';
   data.forEach(ele => {
-    tbody += '<tr>'
-    headers.forEach(header => {
-      tbody += `<td>${ele.hasOwnProperty(header) ? ele[header] : ''}</td>`;
-    });
-    tbody += '</tr>'
+    tbody += `<tr>${buildTableRow(ele, headers)}</tr>`
   });
+  return tbody;
+}
+
+function buildTable(data, headers) {
+  let thead = buildTableHead(headers);
+  
+  let tbody = buildTableBody(data, headers);
 
   let table = `<table>
                 <thead><tr>${thead}</tr></thead>
                 <tbody>${tbody}</tbody>
               </table>`;
-
-  // progress.innerHTML += `<p>DRAWING TABLE...</p>`;
   tableContainer.innerHTML = table;
 }
